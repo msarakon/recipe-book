bf6c7f3dc92f44fbc2d3a6317efb6dff3af44119
Minna Sarakontu <minna.sarakontu@aava.fi>
2020-05-04 21:52:58 +0300

    added styling and layout images

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <div id="app">
-    Hello world
+  <div class="container">
+    <Content />
   </div>
 </template>
 
 <script>
+  import Content from "./Content"
+
   export default {
-    name: "App"
+    name: "App",
+    components: {
+      Content
+    }
   };
 </script>
 
 <style>
+  .container {
+    background-image: url("../assets/bg.jpg");
+    height: 100%;
+  }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="content">
+    <div class="heading">EVOL Recipes ❤️ cooking with LOVE</div>
+    <img class="header" src="../assets/header.jpg" alt="Header" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Content"
+  };
+</script>
+
+<style>
+  :root {
+    --border-color: #A87A4C;
+  }
+
+  .heading {
+    font-family: "Trebuchet MS", sans-serif;
+    font-size: 1.2em;
+    text-align: center;
+    margin-bottom: 1em;
+  }
+
+  .content {
+    box-sizing: border-box;
+    background-color: #fff;
+    width: 100%;
+    max-width: 900px;
+    margin: 0 auto;
+    padding: 1em;
+    height: 100%;
+    box-shadow: 0px 0px 18px 0px var(--border-color);
+  }
+
+  .header {
+    width: 100%;
+  }
+</style>
